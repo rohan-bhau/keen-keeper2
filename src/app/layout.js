e7a0f64fad7bfbe64e97da5e793abaf13c182405
Rohan Mia <rohanmia.org@gmail.com>
2026-04-17 05:07:29 +0600

    Footer created

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/comonents/Navbar/Navbar";
+import Footer from "@/comonents/Footer/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +27,9 @@ export default function RootLayout({ children }) {
     >
       <body className="min-h-full flex flex-col">
         <Navbar />
-        {children}</body>
+        {children}
+      <Footer />
+      </body>
     </html>
   );
 }
---
+++ src/comonents/Footer/Footer.jsx
@@ -0,0 +1,45 @@
+import { BsTwitterX } from "react-icons/bs";
+import { FaFacebookF } from "react-icons/fa";
+import { GrInstagram } from "react-icons/gr";
+import { PiInstagramLogoFill } from "react-icons/pi";
+
+const Footer = () => {
+  return (
+    <div className="bg-[#244D3F] text-center pt-20">
+      <div>
+        <h2 className="text-6xl font-bold text-[#ffffff] mb-4">KeenKeeper</h2>
+        <p className="text-[#ffffff] mb-6 text-md">
+          Your personal shelf of meaningful connections. Browse, tend, and
+          nurture the relationships that matter most.
+        </p>
+        <h2 className="text-[#ffffff] mb-4">Social Links</h2>
+        <ul className="flex justify-center mb-10">
+          <li className="hover:bg-white hover:text-rose-600 rounded-full flex items-center cursor-pointer">
+            <GrInstagram className="w-8 h-8 p-2 text-white hover:text-rose-600" />{" "}
+          </li>
+          <li className="hover:bg-white hover:text-blue-600 rounded-full flex items-center cursor-pointer">
+            <FaFacebookF className="w-8 h-8 p-2 text-white hover:text-blue-600" />{" "}
+          </li>
+          <li className="hover:bg-white hover:text-black rounded-full flex items-center cursor-pointer">
+            <BsTwitterX className="w-8 h-8 p-2 text-white hover:text-black" />{" "}
+          </li>
+        </ul>
+      </div>
+
+      <div className="flex justify-between container mx-auto text-[#ffffff] border-t border-[#1A8862] p-6">
+        <p>&copy; 2026 KeenKeeper. All rights reserved.</p>
+        <ul className="flex gap-3">
+          <li className="hover:text-orange-500 cursor-pointer">
+            Privacy Policy
+          </li>
+          <li className="hover:text-orange-500 cursor-pointer">
+            Teams of Service
+          </li>
+          <li className="hover:text-orange-500 cursor-pointer">Cookies</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
